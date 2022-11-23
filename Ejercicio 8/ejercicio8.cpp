90264ac5897bf2e23a41ad446828ece14b8f551d
tomasClavijo <tmsclvj410@gmail.com>
2022-11-23 09:43:04 -0300

    Update ejercicio8.cpp

--- Ejercicio 8/ejercicio8.cpp
+++ Ejercicio 8/ejercicio8.cpp
@@ -13,6 +13,44 @@ int ** initMatriz(int cantidad, int cota)
     return m;
 }
 
-int main(){
+int ** resolucion(int cantidad, int cota){
+    int** tab = initMatriz(cantidad, cota);
+    for(int i = 1; i <= cantidad; i++){
+        
+    }
+}
+
+int main()
+{
+    // Cargo todos los objetos del conjunto
+    int N;
+    cin >> N;
+    int* objetos = new int [N + 1];
+    objetos[0] = 0;
+    int elemento;
+    for (int i = 1; i < N + 1; i++){
+        cin >> elemento;
+        objetos[i] = elemento;
+    }
+
+    // Cargo la cota
+    int cota;
+    cin >> cota;
+
+    // Cargo las pruebas
+    int P;
+    cin >> P;
+    int* pruebas = new int [P + 1];
+    pruebas[0] = 0;
+    int prueba;
+    for (int i = 1; i < P + 1; i ++){
+        cin >> prueba;
+        pruebas[i] = prueba;
+    }
+
+
+
+
+
     return 0;
 }
