f47aa1ddff202d22081b15791830c129571487e1
Tomás <89207931+tomasClavijo@users.noreply.github.com>
2022-12-06 12:34:08 -0300

    Mochila DAC

---
+++ Ejemplos/Dac_Mochila01.cpp
@@ -0,0 +1,44 @@
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <limits>
+using namespace std;
+
+// A, B, C, D , E
+int valores[] = {4, 2, 10, 1, 2};
+int pesos[] = {12, 1, 4, 1, 2};
+
+int max(int a, int b){
+    return a > b ? a : b;
+}
+
+int mochila01(int objetoHasta, int capacidadRestante){
+	// Problema trivial
+	// Si me quedé sin capacidad o sin objetos para elegir
+	if(capacidadRestante == 0 || objetoHasta < 0){
+		return 0;
+	}
+
+    int valorDelObjeto = valores[objetoHasta];
+    int pesoDelObjeto = pesos[objetoHasta];
+
+    // No puedo poner el objeto
+    if (pesoDelObjeto > capacidadRestante){
+        //Ignoro el objeto
+        return mochila01(objetoHasta-1, capacidadRestante);
+    }
+
+    // Puedo poner el objeto
+    // Calculo el valor de la mochila si pusiera el objeto en consideracion
+    int valorDePoner = valorDelObjeto + mochila01(objetoHasta - 1, capacidadRestante - pesoDelObjeto);
+    // Calculo el valor de la mochila si ignoro el objeto
+    int valorDeNoPoner = mochila01(objetoHasta - 1, capacidadRestante) ;
+
+    return max(valorDePoner, valorDeNoPoner);
+}
+
+int main(){
+	int capacidadMochila = 15;
+    cout << mochila01(4, capacidadMochila) << endl;
+	return 0;
+}
